c67ffa17db52183b9447b5fa0d87bbf1fa22d5c0
Eric Mesa <ericsbinaryworld@gmail.com>
2020-04-27 16:57:15 -0400

    Fixed issue with failing test.
    I think my variable was being treated as a class variable instead of a self variable.

--- prophecypracticum/engine/controller.py
+++ prophecypracticum/engine/controller.py
@@ -8,7 +8,8 @@ from . import prophecy
 
 
 class Controller:
-    users: typing.List[user.User] = []
+    def __init__(self):
+        self.users: typing.List[user.User] = []
 
     def create_user(self, username: str, user_email: str, user_id: int) -> None:
         """Create a user and add it to the users list.
@@ -26,6 +27,11 @@ class Controller:
         :param prophet_id: user_id for the prophet - should exist in users.
         :param supplicant_id: user_id for the supplicant - should exist in users.
         """
+        print(supplicant_id)
+        print(len(self.users))
+        for supplicant in self.users:
+            print(supplicant)
+
         for prophet in self.users:
             if prophet.my_id == prophet_id and any(supplicant.my_id == supplicant_id for supplicant in self.users):
                 prophet.supplicant_id = supplicant_id
